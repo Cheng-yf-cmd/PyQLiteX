35cd5f168fd23c67f70fb44f45a55ba8c81b4a48
Tiger1218 <tiger1218@foxmail.com>
2023-04-16 05:09:08 +0800

    Version 0.1.1 Released

--- pyqlitex/__init__.py
+++ pyqlitex/__init__.py
@@ -1,7 +1,7 @@
 '''
 # TODO: to be construct, commented by Tiger1218<tiger1218@foxmail.com>
 '''
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["codeql", "basic"]
 # from .basic.project import PythonProject
 from pyqlitex.codeql import codeql_wrapper
